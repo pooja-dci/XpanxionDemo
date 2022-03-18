197ac8c32385c41836bfaa8cf44f190021bce55b
Jonathan Ocampo <88061312+NoirPhantom@users.noreply.github.com>
2022-03-18 16:20:19 -0500

    Change
    Created a file with name and  added a line on the java file

--- EveryOneModifies.java
+++ EveryOneModifies.java
@@ -1,8 +1,9 @@
 public class EveryOneModifies {
-  
+
   public static void main(String[] args) {
-    
-      System.out.println("Pooja Purohit");
+
+    System.out.println("Pooja Purohit");
+
+    System.out.println("Jonathan Ocampo");
   }
 }
-
