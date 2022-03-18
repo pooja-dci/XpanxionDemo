5c0b1c0990b71d39a42fb620dc59db17e8863747
Anatalio Nickles <99271743+AnatalioNickles@users.noreply.github.com>
2022-03-18 16:15:58 -0500

    Update EveryOneModifies.java
    yep, altered.

--- EveryOneModifies.java
+++ EveryOneModifies.java
@@ -3,6 +3,8 @@ public class EveryOneModifies {
   public static void main(String[] args) {
     
       System.out.println("Pooja Purohit");
+    System.out.println("The Amazing Anatalio wants a piece of this project!");
+    
   }
 }
 
